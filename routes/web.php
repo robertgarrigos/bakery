5bf0535aee928c7b803f44d5794fb97901ef11e0
robertgarrigos <robert@garrigos.cat>
2020-05-13 18:30:11 +0200

    disable registration

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin', 'AdminController@index')->name('admin');
